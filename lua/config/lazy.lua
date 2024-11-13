8af0f829cbde438707583af1374462ed08fec6da
Narong-Kanthanu <narong.okan@gmail.com>
2024-11-13 11:55:26 +0700

    (fix): Remove coding.copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,7 +28,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
     { import = "plugins.lsp" },
