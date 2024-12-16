b2c7447d13c1174c756fdc8602cf034fb6febbaf
Narong-Kanthanu <narong.okan@gmail.com>
2024-12-16 19:37:19 +0700

    (chore): Update moice config

--- lua/plugins/editor/ui.lua
+++ lua/plugins/editor/ui.lua
@@ -51,6 +51,7 @@ return {
   -- messages, cmdline and the popupmenu
   {
     "folke/noice.nvim",
+    event = "VeryLazy",
     opts = function(_, opts)
       table.insert(opts.routes, {
         filter = {
