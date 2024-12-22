eb2a12e28ab60522e31fbcf65e9ecf481e216880
Narong-Kanthanu <narong.okan@gmail.com>
2024-12-22 14:00:51 +0700

    (fix): Update blink ui config and experience

--- lua/plugins/lsp/cmp-config.lua
+++ lua/plugins/lsp/cmp-config.lua
@@ -1,6 +1,11 @@
 return {
   {
     "saghen/blink.cmp",
+    opts_extend = {
+      "sources.completion.enabled_providers",
+      "sources.compat",
+      "sources.default",
+    },
     dependencies = {
       "rafamadriz/friendly-snippets",
       {
@@ -16,21 +21,66 @@ return {
     event = "InsertEnter",
     opts = {
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release
-        use_nvim_cmp_as_default = true,
+        use_nvim_cmp_as_default = false,
+        kind_icons = {
+          Copilot = "",
+          Text = "󰉿",
+          Method = "󰊕",
+          Function = "󰊕",
+          Constructor = "󰒓",
+
+          Field = "󰜢",
+          Variable = "󰆦",
+          Property = "󰖷",
+
+          Class = "󱡠",
+          Interface = "󱡠",
+          Struct = "󱡠",
+          Module = "󰅩",
+
+          Unit = "󰪚",
+          Value = "󰦨",
+          Enum = "󰦨",
+          EnumMember = "󰦨",
+
+          Keyword = "󰻾",
+          Constant = "󰏿",
+
+          Snippet = "󱄽",
+          Color = "󰏘",
+          File = "󰈔",
+          Reference = "󰬲",
+          Folder = "󰉋",
+          Event = "󱐋",
+          Operator = "󰪚",
+          TypeParameter = "󰬛",
+        },
       },
       sources = {
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
         providers = {
           copilot = {
             name = "copilot",
             module = "blink-cmp-copilot",
+            score_offset = 100,
+            async = true,
+            transform_items = function(_, items)
+              local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+              local kind_idx = #CompletionItemKind + 1
+              CompletionItemKind[kind_idx] = "Copilot"
+              for _, item in ipairs(items) do
+                item.kind = kind_idx
+              end
+              return items
+            end,
           },
         },
-        completion = {
-          enabled_providers = { "lsp", "path", "snippets", "buffer", "copilot" },
-        },
+        compat = {},
+      },
+      snippets = {
+        expand = function(snippet, _)
+          return LazyVim.cmp.expand(snippet)
+        end,
       },
       completion = {
         documentation = {
@@ -39,7 +89,7 @@ return {
           treesitter_highlighting = false,
           window = {
             border = "rounded",
-            padding = 0,
+            winblend = 0,
             winhighlight = "Normal:BorderBG,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
           },
         },
@@ -53,7 +103,7 @@ return {
             treesitter = {},
           },
           border = "rounded",
-          padding = 0,
+          winblend = 0,
           winhighlight = "Normal:BorderBG,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
         },
         accept = {
