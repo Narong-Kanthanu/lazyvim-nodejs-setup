7ca3b92ca502342f6a854d111c0dc22ba418dbf9
Narong-Kanthanu <narong.okan@gmail.com>
2025-01-02 00:47:20 +0700

    (feat): Update which-key support custom keymap

--- lua/plugins/other/which-key.lua
+++ lua/plugins/other/which-key.lua
@@ -1,23 +1,32 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {},
-  keys = {
-    {
-      "<Leader>ff",
-      mode = "n",
-      function()
-        vim.cmd(":NvimTreeFindFileToggle<Return>")
-      end,
-      desc = "File explorer",
-    },
-    {
-      "<Leader>ca",
-      mode = { "n", "v" },
-      function()
-        vim.lsp.buf.code_action()
-      end,
-      desc = "Code Actions",
-    },
-  },
+  config = function()
+    local wk = require("which-key")
+    wk.setup({
+      preset = "modern",
+      show_help = false,
+      show_keys = false,
+      win = {
+        border = "rounded",
+        title = false,
+      },
+    })
+    wk.add({
+      {
+        "<Leader>ff",
+        mode = "n",
+        "<cmd>NvimTreeFindFileToggle<cr>",
+        desc = "File explorer",
+      },
+      {
+        "<Leader>ca",
+        mode = { "n", "v" },
+        function()
+          vim.lsp.buf.code_action()
+        end,
+        desc = "Code Actions",
+      },
+    })
+  end,
 }
