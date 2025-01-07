01adea52d6ff4c6b508f1c5b77935cdad0c3491a
Narong-Kanthanu <narong.okan@gmail.com>
2025-01-07 19:16:21 +0700

    (feat): Update lazygit add telescope for handle submodule

--- lua/plugins/git/lazygit.lua
+++ lua/plugins/git/lazygit.lua
@@ -2,10 +2,14 @@ return {
   -- LazyGit integration with Telescope
   {
     "kdheepak/lazygit.nvim",
+    lazy = true,
     keys = {},
-    -- optional for floating window border decoration
     dependencies = {
+      "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
     },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
   },
 }
