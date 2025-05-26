dd487e578a647ef595e4c79d6e6105d25cd17520
Narong-Kanthanu <narong.okan@gmail.com>
2025-05-26 13:49:11 +0700

    (chore): Move out snacks of ui.lua and create new file for it's
    More config later and instance of telescope soon....

---
+++ lua/plugins/editor/snacks.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      scroll = { enabled = false },
+      image = {
+        enabled = true,
+        border = "rounded",
+        img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments" },
+        formats = {
+          "png",
+          "jpg",
+          "jpeg",
+          "gif",
+          "bmp",
+          "webp",
+          "tiff",
+          "heic",
+          "avif",
+        },
+        icons = {
+          math = "󰪚 ",
+          chart = "󰄧 ",
+          image = " ",
+        },
+        doc = {
+          enabled = false,
+          inline = true,
+          float = true,
+          max_width = 80,
+          max_height = 40,
+        },
+        math = {
+          enabled = false,
+        },
+        convert = {
+          notify = true,
+          mermaid = function()
+            local theme = vim.o.background == "light" and "neutral" or "dark"
+            return { "-i", "{src}", "-o", "{file}", "-b", "transparent", "-t", theme, "-s", "{scale}" }
+          end,
+          magick = {
+            default = { "{src}[0]", "-scale", "1920x1080>" }, -- default for raster images
+            vector = { "-density", 192, "{src}[0]" }, -- used by vector images like svg
+            math = { "-density", 192, "{src}[0]", "-trim" },
+          },
+        },
+      },
+    },
+  },
+}
