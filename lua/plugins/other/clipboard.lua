1ffe80e318e5d92c4fa3bfba3b594a351b9fc402
Narong Kanthanu <narong.okan@gmail.com>
2025-06-11 14:26:06 +0700

    (feat): Update clipboard opts

--- lua/plugins/other/clipboard.lua
+++ lua/plugins/other/clipboard.lua
@@ -4,8 +4,9 @@ return {
     config = function()
       require("osc52").setup({
         max_length = 0, -- No limit
-        silent = false,
-        trim = false,
+        silent = true,
+        trim = true,
+        tmux_passthrough = true,
       })
     end,
   },
