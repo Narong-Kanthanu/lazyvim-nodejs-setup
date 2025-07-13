f7332715540569c925a8deca10bf004d7d4650c4
Narong-Kanthanu <narong.okan@gmail.com>
2025-07-13 10:06:52 +0700

    (chore): Enhance eslint config

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -39,6 +39,25 @@ return {
               "typescript",
               "javascript",
             },
+            root_dir = function(bufnr, on_dir)
+              local root_file_patterns = {
+                ".eslintrc",
+                ".eslintrc.js",
+                ".eslintrc.cjs",
+                ".eslintrc.yaml",
+                ".eslintrc.yml",
+                ".eslintrc.json",
+                "eslint.config.js",
+                "eslint.config.mjs",
+                "eslint.config.cjs",
+                "eslint.config.ts",
+                "eslint.config.mts",
+                "eslint.config.cts",
+              }
+              local fname = vim.api.nvim_buf_get_name(bufnr)
+              root_file_patterns = util.insert_package_json(root_file_patterns, "eslintConfig", fname)
+              on_dir(vim.fs.dirname(vim.fs.find(root_file_patterns, { path = fname, upward = true })[1]))
+            end,
             settings = {
               workingDirectories = { mode = "auto" },
             },
