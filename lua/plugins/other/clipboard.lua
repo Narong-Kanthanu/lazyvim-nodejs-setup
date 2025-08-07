b996f5ab8089ad47b8d8d2b85e83ae3a1dbea257
Narong Kanthanu <narong.okan@gmail.com>
2025-08-07 10:31:22 +0700

    (chore): Format code

--- lua/plugins/other/clipboard.lua
+++ lua/plugins/other/clipboard.lua
@@ -1,13 +1,11 @@
 return {
-  {
-    "ojroques/nvim-osc52",
-    config = function()
-      require("osc52").setup({
-        max_length = 0, -- No limit
-        silent = true,
-        trim = false,
-        tmux_passthrough = true,
-      })
-    end,
-  },
+  "ojroques/nvim-osc52",
+  config = function()
+    require("osc52").setup({
+      max_length = 0, -- No limit
+      silent = true,
+      trim = false,
+      tmux_passthrough = true,
+    })
+  end,
 }
