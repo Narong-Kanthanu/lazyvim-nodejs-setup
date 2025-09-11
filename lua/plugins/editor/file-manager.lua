b5abf3003a8f4b73e279fe10a535914e1c25c983
Narong-Kanthanu <narong.okan@gmail.com>
2025-09-11 10:55:20 +0700

    (chore): remap file manager

--- lua/plugins/editor/file-manager.lua
+++ lua/plugins/editor/file-manager.lua
@@ -21,9 +21,9 @@ return {
       keymaps = {
         show_help = "<f1>",
         open_file_in_vertical_split = "<c-v>",
-        open_file_in_horizontal_split = "<c-x>",
+        open_file_in_horizontal_split = "<c-s>",
         open_file_in_tab = "<c-t>",
-        grep_in_directory = "<c-s>",
+        grep_in_directory = "<c-f>",
         replace_in_directory = "<c-g>",
         cycle_open_buffers = "<tab>",
         copy_relative_path_to_selected_files = "<c-y>",
