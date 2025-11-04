aa0c884f12307ca02cfa58f17501e1b8f9ea5f79
Narong Kanthanu <narong.okan@gmail.com>
2025-11-04 12:05:28 +0700

    (chore): Update preview MD file

---
+++ lua/plugins/other/peek.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup({
+        auto_load = true,
+        close_on_bdelete = true,
+        syntax = true,
+        theme = "dark",
+        update_on_change = true,
+        app = "webview", -- 'webview', 'browser', string or a table of strings
+        filetype = { "markdown" },
+        throttle_at = 200000,
+        throttle_time = "auto",
+      })
+    end,
+    keys = {
+      { "<leader>m", "", desc = "Markdown", mode = { "n" } },
+      {
+        "<Leader>md",
+        function()
+          require("peek").open()
+        end,
+        desc = "󰈔 Preview markdown",
+        mode = { "n" },
+      },
+      {
+        "<Leader>mD",
+        function()
+          require("peek").close()
+        end,
+        desc = "󰈔 Stop preview markdown",
+        mode = { "n" },
+      },
+    },
+  },
+}
