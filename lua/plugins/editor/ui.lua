fc78ada6de1709463594fa64ea98b0f9f1a6397a
Narong Kanthanu <narong.okan@gmail.com>
2026-02-15 16:03:45 +0700

    (chore): Disable neo-tree and use nvim-tree instead

--- lua/plugins/editor/ui.lua
+++ lua/plugins/editor/ui.lua
@@ -4,6 +4,8 @@ return {
     "nvimdev/dashboard-nvim",
     enabled = false,
   },
+  -- neo-tree
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- status line
   {
     "nvim-lualine/lualine.nvim",
