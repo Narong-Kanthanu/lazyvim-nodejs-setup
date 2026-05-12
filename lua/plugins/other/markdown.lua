a178dc1c0f73708726517b399651b9f35909c149
Narong Kanthanu <narong.okan@gmail.com>
2026-05-12 18:25:20 +0700

    (chore): Remove obsidian.nvim plugin

--- lua/plugins/other/markdown.lua
+++ lua/plugins/other/markdown.lua
@@ -50,19 +50,6 @@ return {
     },
   },
   {
-    "obsidian-nvim/obsidian.nvim",
-    version = "*", -- use latest release, remove to use latest commit
-    ft = "markdown",
-    cmd = { "Obsidian" },
-    opts = {
-      legacy_commands = false, -- this will be removed in the next major release
-      sync = {
-        enabled = false,
-      },
-      workspaces = workspaces,
-    },
-  },
-  {
     "Narong-Kanthanu/llm-kiwi.nvim",
     version = "*", -- use latest release, remove to use latest commit
     cmd = { "LlmKiwiOpen", "LlmKiwiClose", "LlmKiwiList" },
